0204017d89611c20074cae2c716ba5d84bd353c4
jp <johnamata@protonmail.com>
2022-07-13 23:06:31 +0800

    implement add method for records

--- src/main/java/com/dbserver/raw/FileHandler.java
+++ src/main/java/com/dbserver/raw/FileHandler.java
@@ -3,9 +3,10 @@ package com.dbserver.raw;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
 
 /**
- * currently, just for opening and closing database files
+ * opening + closing db files, storing records
  */
 public class FileHandler {
     private RandomAccessFile dbFile;
@@ -13,7 +14,35 @@ public class FileHandler {
         this.dbFile = new RandomAccessFile(dbFileName, "rw"); //rw: read, write
     }
     public boolean add(String name, int age, String address, String carPlateNumber, String description) throws IOException {
-        this.dbFile.seek(this.dbFile.length());
+        this.dbFile.seek(this.dbFile.length()); //seek to end o fifle
+        //length of record
+        int length = 4 +
+                name.length()
+                + 4
+                + 4
+                + address.length()
+                + 4
+                + carPlateNumber.length()
+                + 4 + description.length();
+        //store if deleted
+        this.dbFile.writeBoolean(false);
+        //store record length
+        this.dbFile.writeInt(length);;
+        //store name
+        this.dbFile.writeInt(name.length());
+        this.dbFile.write(name.getBytes());
+        //store age
+        this.dbFile.writeInt(age);
+        //store address
+        this.dbFile.writeInt(address.length());
+        this.dbFile.write(address.getBytes());
+        //store plate
+        this.dbFile.writeInt(carPlateNumber.length());
+        this.dbFile.write(carPlateNumber.getBytes());
+        //store description
+        this.dbFile.writeInt(description.length());
+        this.dbFile.write(description.getBytes());
+        return true;//if were able to store data
     }
 
     public void close() throws IOException { //for closing DB file
